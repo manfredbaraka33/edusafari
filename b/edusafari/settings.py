78573657aeb5b63152b012e6a6b82d28edd5fc29
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-10-06 12:52:21 +0300

    Update settings.py

--- b/edusafari/settings.py
+++ b/edusafari/settings.py
@@ -81,12 +81,14 @@ CORS_ALLOWED_ORIGINS = [
    
      "http://127.0.0.1:5173",
      "http://localhost:5173",
+    'edusafari.onrender.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     
     "http://127.0.0.1:5173",
-     "http://localhost:5173"
+     "http://localhost:5173",
+    'edusafari.onrender.com'
      ]
 
 CORS_ALLOW_CREDENTIALS = True
