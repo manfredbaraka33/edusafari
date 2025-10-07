02caa81ccdd32f2fa6df1836c5b64870b380bea4
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-10-07 14:35:55 +0300

    Update PlatformDetail.jsx

--- f/src/pages/PlatformDetail.jsx
+++ f/src/pages/PlatformDetail.jsx
@@ -52,7 +52,7 @@ export default function PlatformDetail() {
     }
   };
 
-  if (!platform) return <p className="text-center text-gray-500">Loading...</p>;
+  if (!platform) return <p className="text-center text-gray-500 p-30">Loading...</p>;
 
   return (
     <div className="max-w-3xl mx-auto p-6">
