50f64094d4277852a644ec6671e75078b8f1acc6
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-10-07 13:38:08 +0300

    Update AddVendorService.jsx

--- f/src/components/AddVendorService.jsx
+++ f/src/components/AddVendorService.jsx
@@ -42,9 +42,9 @@ export default function AddVendorService({isOpen,onClose}) {
   return (
     <div className="fixed inset-0 bg-white dark:bg-gray-800 bg-opacity-40 flex items-center justify-center z-50">
     <div className="max-w-md mx-auto p-6">
-      <div className="flex justify-between"><h2 className="text-2xl font-bold mb-6 dark:text-white">Add Vendor Service</h2>
+      <div className="flex justify-between overflow-y-auto"><h2 className="text-2xl font-bold mb-6 dark:text-white">Add Vendor Service</h2>
       <XIcon onClick={onClose} className="text-red-500 cursor-pointer" /></div>
-      <form onSubmit={handleSubmit} className="space-y-4 bg-white dark:bg-gray-800 p-6 rounded-lg shadow overflow-y-auto">
+      <form onSubmit={handleSubmit} className="space-y-4 bg-white dark:bg-gray-800 p-6 rounded-lg shadow ">
         <input
           type="text"
           name="name"
